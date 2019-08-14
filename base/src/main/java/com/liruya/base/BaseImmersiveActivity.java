473749182e283bbe373fbe89e081818ffe925ae5
liruya <cylary1218@gmail.com>
2019-08-14 18:04:08 +0800

    修复沉浸式页面初始状态栏不显示的问题

--- base/src/main/java/com/liruya/base/BaseImmersiveActivity.java
+++ base/src/main/java/com/liruya/base/BaseImmersiveActivity.java
@@ -4,16 +4,12 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
 
 public abstract class BaseImmersiveActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            View decorView = getWindow().getDecorView();
-            int option = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-            decorView.setSystemUiVisibility(option);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
 
